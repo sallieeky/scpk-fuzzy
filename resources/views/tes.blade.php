d04cc545a80ca5e262273beb645546fa5907e4e9
sallieeky <sallieeky@gmail.com>
2022-05-07 10:43:13 +0800

    init commit

---
+++ resources/views/tes.blade.php
@@ -0,0 +1,18 @@
+@extends("layouts.base")
+@section("content")
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <h6 class="m-0 font-weight-bold text-primary">Tes</h6>
+        </div>
+        <div class="card-body">
+          
+        </div>
+    </div>
+  </div>
+</div>
+
+@endsection
