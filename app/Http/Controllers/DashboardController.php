49f55db673a33dbabad4b4683daa3d0c0d6573ca
sallieeky <sallieeky@gmail.com>
2022-05-08 17:52:29 +0800

    fuzzy count

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,310 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function tes()
+    {
+        // $data = $this->fuzzySugeno();
+        $data = $this->rank();
+        // return $data;
+        return view('tes', compact('data'));
+    }
+
+    public function fuzzySugeno()
+    {
+        $mahasiswa = Mahasiswa::all();
+        // IPK
+        $ipk = $this->countIpk($mahasiswa);
+        // Lama Studi
+        $lama_studi = $this->countLamaStudi($mahasiswa);
+        // SK2PM
+        $sk2pm = $this->countSk2pm($mahasiswa);
+        // TOEFL
+        $toefl = $this->countToefl($mahasiswa);
+
+        $data = [];
+        for ($i = 0; $i < count($mahasiswa); $i++) {
+            $data[$i]['id'] = $mahasiswa[$i]->id;
+            $data[$i]['nama'] = $mahasiswa[$i]->nama;
+            $data[$i]['ipk'] = $ipk[$i];
+            $data[$i]['lama_studi'] = $lama_studi[$i];
+            $data[$i]['sk2pm'] = $sk2pm[$i];
+            $data[$i]['toefl'] = $toefl[$i];
+        }
+
+        $bobot = [
+            "tinggi" => 10,
+            "sedang" => 7,
+            "rendah" => 3,
+        ];
+
+        $keluaran = [];
+        $itt = 0;
+        foreach ($data as $dt) {
+            $keluaranTempPem = 0;
+            $keluaranTempPen = 0;
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]) * $bobot["tinggi"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["rendah"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]) * $bobot["tinggi"];
+                $keluaranTempPen += min($dt['ipk']["rendah"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["lama"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]) * $bobot["tinggi"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["lama"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["rendah"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["rendah"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]) * $bobot["rendah"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["sedang"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]) * $bobot["sedang"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["sedang"], $dt["toefl"]["tinggi"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["rendah"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]) * $bobot["tinggi"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["rendah"]);
+            }
+            if ($dt['ipk']["tinggi"] != 0 && $dt["lama_studi"]["cepat"] != 0 && $dt["sk2pm"]["tinggi"] != 0 && $dt["toefl"]["tinggi"] != 0) {
+                $keluaranTempPem += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]) * $bobot["tinggi"];
+                $keluaranTempPen += min($dt['ipk']["tinggi"], $dt["lama_studi"]["cepat"], $dt["sk2pm"]["tinggi"], $dt["toefl"]["tinggi"]);
+            }
+            $hasil = $keluaranTempPem / $keluaranTempPen;
+            $keluaran[$itt]['pem'] = $keluaranTempPem;
+            $keluaran[$itt]['pen'] = $keluaranTempPen;
+            $keluaran[$itt]['hasil'] = $hasil;
+            $itt++;
+        }
+
+        $result = [];
+        for ($i = 0; $i < count($mahasiswa); $i++) {
+            $result[$i]['id'] = $mahasiswa[$i]->id;
+            $result[$i]['nama'] = $mahasiswa[$i]->nama;
+            $result[$i]["nilai"] = $keluaran[$i]['hasil'];
+        }
+
+        return $result;
+    }
+
+    public function rank()
+    {
+        $data = $this->fuzzySugeno();
+        usort($data, function ($a, $b) {
+            return $a['nilai'] < $b['nilai'];
+        });
+        return $data;
+    }
+
+    public function countIpk($mahasiswa)
+    {
+        $nilai = [];
+
+        foreach ($mahasiswa as $mhs) {
+            $nilaiTemp = [];
+            $nilaiTemp['nama'] = $mhs->nama;
+            if ($mhs->ipk <= 3) {
+                $nilaiTemp['rendah'] = 1;
+                $nilaiTemp['tinggi'] = 0;
+            } else if ($mhs->ipk > 3 && $mhs->ipk < 3.5) {
+                $resultRendah = (3.5 - $mhs->ipk) / (3.5 - 3);
+                $nilaiTemp['rendah'] = $resultRendah;
+
+                $resultTinggi = ($mhs->ipk - 3) / (4 - 3);
+                $nilaiTemp['tinggi'] = $resultTinggi;
+            } else if ($mhs->ipk >= 3.5 && $mhs->ipk < 4) {
+                $nilaiTemp['rendah'] = 0;
+                $resultTinggi = ($mhs->ipk - 3) / (4 - 3);
+                $nilaiTemp['tinggi'] = $resultTinggi;
+            } else if ($mhs->ipk == 4) {
+                $nilaiTemp['tinggi'] = 1;
+                $nilaiTemp['rendah'] = 0;
+            }
+            $nilai[] = $nilaiTemp;
+        }
+
+        return $nilai;
+    }
+
+    public function countLamaStudi($mahasiswa)
+    {
+        $nilai = [];
+
+        foreach ($mahasiswa as $mhs) {
+            $nilaiTemp = [];
+
+            if ($mhs->lama_studi <= 48) {
+                $nilaiTemp['cepat'] = 1;
+                $nilaiTemp['lama'] = 0;
+            } else if ($mhs->lama_studi > 48 && $mhs->lama_studi < 56) {
+                $resultCepat = (56 - $mhs->lama_studi) / (56 - 48);
+                $nilaiTemp['cepat'] = $resultCepat;
+
+                $resultLama = ($mhs->lama_studi - 48) / (72 - 48);
+                $nilaiTemp['lama'] = $resultLama;
+            } else if ($mhs->lama_studi >= 56 && $mhs->lama_studi < 72) {
+                $nilaiTemp['cepat'] = 0;
+                $resultLama = ($mhs->lama_studi - 48) / (72 - 48);
+                $nilaiTemp['lama'] = $resultLama;
+            } else if ($mhs->lama_studi >= 72) {
+                $nilaiTemp['lama'] = 1;
+                $nilaiTemp['cepat'] = 0;
+            }
+            $nilai[] = $nilaiTemp;
+        }
+
+        return $nilai;
+    }
+
+    public function countSk2pm($mahasiswa)
+    {
+        $nilai = [];
+
+        foreach ($mahasiswa as $mhs) {
+            $nilaiTemp = [];
+
+            if ($mhs->sk2pm <= 3000) {
+                $nilaiTemp['rendah'] = 1;
+                $nilaiTemp['sedang'] = 0;
+                $nilaiTemp['tinggi'] = 0;
+            } else if ($mhs->sk2pm > 3000 && $mhs->sk2pm <= 5000) {
+                $resultRendah = (5000 - $mhs->sk2pm) / (5000 - 3000);
+                $nilaiTemp['rendah'] = $resultRendah;
+                $nilaiTemp['sedang'] = 0;
+
+                if ($mhs->sk2pm >= 4000 && $mhs->sk2pm <= 5000) {
+                    $resultSedang = ($mhs->sk2pm - 4000) / (7000 - 4000);
+                    $nilaiTemp['sedang'] = $resultSedang;
+                }
+                $nilaiTemp['tinggi'] = 0;
+            } else if ($mhs->sk2pm > 5000 && $mhs->sk2pm <= 7000) {
+                $nilaiTemp['rendah'] = 0;
+                $resultSedang = ($mhs->sk2pm - 4000) / (7000 - 4000);
+                $nilaiTemp['sedang'] = $resultSedang;
+                $nilaiTemp['tinggi'] = 0;
+            } else if ($mhs->sk2pm > 7000 && $mhs->sk2pm <= 9000) {
+                $nilaiTemp['rendah'] = 0;
+                $resultSedang = (10000 - $mhs->sk2pm) / (10000 - 7000);
+                $nilaiTemp['sedang'] = $resultSedang;
+                $nilaiTemp['tinggi'] = 0;
+            } else if ($mhs->sk2pm > 9000) {
+                $nilaiTemp['rendah'] = 0;
+                $resultTinggi = ($mhs->sk2pm - 9000) / (12000 - 9000);
+                $nilaiTemp['tinggi'] = $resultTinggi;
+                $nilaiTemp['sedang'] = 0;
+                if ($mhs->sk2pm > 9000 && $mhs->sk2pm <= 10000) {
+                    $resultSedang = (10000 - $mhs->sk2pm) / (10000 - 7000);
+                    $nilaiTemp['sedang'] = $resultSedang;
+                }
+                if ($mhs->sk2pm >= 12000) {
+                    $nilaiTemp['tinggi'] = 1;
+                }
+            }
+            $nilai[] = $nilaiTemp;
+        }
+        return $nilai;
+    }
+
+    public function countToefl($mahasiswa)
+    {
+        $nilai = [];
+
+        foreach ($mahasiswa as $mhs) {
+            $nilaiTemp = [];
+
+            if ($mhs->toefl <= 300) {
+                $nilaiTemp['rendah'] = 1;
+                $nilaiTemp['tinggi'] = 0;
+            } else if ($mhs->toefl > 300 && $mhs->toefl <= 500) {
+                $resultRendah = (500 - $mhs->toefl) / (500 - 300);
+                $nilaiTemp['rendah'] = $resultRendah;
+
+                $resultTinggi = ($mhs->toefl - 400) / (677 - 400);
+                $nilaiTemp['tinggi'] = $resultTinggi;
+                if ($mhs->toefl < 400) {
+                    $nilaiTemp['tinggi'] = 0;
+                }
+            } else if ($mhs->toefl > 500 && $mhs->toefl <= 677) {
+                $nilaiTemp['rendah'] = 0;
+                $resultTinggi = ($mhs->toefl - 400) / (677 - 400);
+                $nilaiTemp['tinggi'] = $resultTinggi;
+            }
+            $nilai[] = $nilaiTemp;
+        }
+        return $nilai;
+    }
+}
---
+++ database/migrations/2022_05_08_002640_create_mahasiswas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama");
+            $table->string("nim");
+            $table->string("prodi");
+
+            $table->float("ipk");
+            $table->integer("lama_studi");
+            $table->integer("sk2pm");
+            $table->integer("toefl");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
